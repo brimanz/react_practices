a85c9ad501b7838fa8357cd6d7dac3b34eedb753
Brimanz <brimanz2014@gmail.com>
2021-10-03 19:46:29 -0400

    exercise_2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import {DISHES} from './shared/dishes'
 
 //component imports
 import Menu from './components/MenuComponent';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="info">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante con fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <Navbar dark color="info">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante con fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  } 
 }
 
 export default App;
